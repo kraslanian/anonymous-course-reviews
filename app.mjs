63165c98aa537b423fdc1b24b51ed64445610a59
Kristina <ks6304@nyu.edu>
2023-10-30 13:06:57 -0400

    port runs, database runs

--- app.mjs
+++ app.mjs
@@ -1,5 +1,14 @@
-import express from 'express';
+import express from "express";
+import { config } from "dotenv";
+import Review from "./db.mjs";
+
 const app = express();
+
+// configure templating to hbs
+app.set('view engine', 'hbs');
+
+//const Review = mongoose.model("Review")
+//const Review = mongoose.model("Review");
 
 // set up express static
 import url from 'url';
@@ -7,14 +16,25 @@ import path from 'path';
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// configure templating to hbs
-app.set('view engine', 'hbs');
+
 
 // body parser (req.body)
 app.use(express.urlencoded({ extended: false }));
 
+
+
 app.get('/', (req, res) => {
-  res.send('TODO: add / modify routes')
+
+  Review.find({})
+  .then((reviews) => {
+    res.render("displayreviews", {reviews});
+    })
+
+    .catch(error => {
+      res.status(500).send('Error fetching reviews from the database');
+    });
 });
 
+
+console.log((`Started on port: ${process.env.PORT}`))
 app.listen(process.env.PORT || 3000);
--- db.mjs
+++ db.mjs
@@ -1,1 +1,26 @@
 // add your code here!
+import mongoose from "mongoose";
+import {config} from "dotenv";
+config();
+
+// my schema goes here!
+
+const ReviewSchema = new mongoose.Schema({
+	name: String,
+	updated_at: Date,
+    number: String,
+    name: String,
+    semester: String,
+    year: Number,
+    professor: String, 
+    review: String
+})
+
+
+const Review = mongoose.model("Review", ReviewSchema);
+
+// Uncomment following line to debug value of database connectoin string
+console.log(`Connecting to database at: ${process.env.DSN}`);
+mongoose.connect(process.env.DSN);
+
+export default Review;
